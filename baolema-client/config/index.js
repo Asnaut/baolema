388b8ce037b963162a6bd4e64a2b9a71dcd7089c
Asnaut <unration@outlook.com>
2019-06-11 08:56:09 +0800

    api fix

--- baolema-client/config/index.js
+++ baolema-client/config/index.js
@@ -19,10 +19,10 @@ module.exports = {
       '/api': { // 匹配所有以 '/api'开头的请求路径
         target: 'http://localhost:4000', // 代理目标的基础路径
         // secure: false,  // 如果是https接口，需要配置这个参数
-        changeOrigin: true, // 支持跨域
-        pathRewrite: { // 重写路径: 去掉路径中开头的'/api'
-          '^/api': ''
-        }
+        changeOrigin: true // 支持跨域
+        // pathRewrite: { // 重写路径: 去掉路径中开头的'/api'
+        //   '^/api': ''
+        // }
       }
     },
 
@@ -64,7 +64,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
